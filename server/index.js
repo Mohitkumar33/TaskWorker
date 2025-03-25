c90e2de79621ca7c60da9da3f8d77dbac59e7411
Mohit Aggarwal <mohitkumar06612@gmail.com>
2025-03-26 09:03:19 +1100

    feat: added login and register apis

--- server/index.js
+++ server/index.js
@@ -1,5 +1,19 @@
+require("dotenv").config();
 const express = require("express");
+const connectDB = require("./config/db");
+
+
 const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Connect to Database
+connectDB();
+
+// Routes
+app.use("/api/auth", require("./routes/authRoutes"));
+
 const port = 3300;
 
 app.get("/", (req, res) => {
---
+++ server/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["user", "provider", "admin"], default: "user" },
+  },
+  { timestamps: true }
+);
+
+// Hash password before saving to DB
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Compare Password for Login
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model("User", UserSchema);
