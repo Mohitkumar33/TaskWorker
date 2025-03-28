24c2751cf0ea9acf6be80edc11f78b46103568da
Mohit Aggarwal <mohitkumar06612@gmail.com>
2025-03-28 14:48:53 +1100

    feat: added role based access

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -2,7 +2,8 @@ const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
   const token = req.header("Authorization");
-  if (!token) return res.status(401).json({ msg: "No token, authorization denied" });
+  if (!token)
+    return res.status(401).json({ msg: "No token, authorization denied" });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
@@ -13,4 +14,16 @@ const authMiddleware = (req, res, next) => {
   }
 };
 
-module.exports = authMiddleware;
+// Middleware to check role permissions
+const authorizeRoles = (...allowedRoles) => {
+  return (req, res, next) => {
+    if (!allowedRoles.includes(req.user.role)) {
+      return res
+        .status(403)
+        .json({ message: "Forbidden: You do not have permission" });
+    }
+    next();
+  };
+};
+
+module.exports = { authMiddleware, authorizeRoles };
