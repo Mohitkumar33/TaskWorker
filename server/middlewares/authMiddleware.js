577308a0ca58a60ac4ef33787384fdeb95fe3792
Mohit Kumar <46507817+Mohitkumar33@users.noreply.github.com>
2025-03-28 14:50:19 +1100

    feat: added login api, register api and Jwt (#2)
    * feat: added login and register apis
    
    * feat: added role based access

---
+++ server/middlewares/authMiddleware.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = (req, res, next) => {
+  const token = req.header("Authorization");
+  if (!token)
+    return res.status(401).json({ msg: "No token, authorization denied" });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: "Invalid token" });
+  }
+};
+
+// Middleware to check role permissions
+const authorizeRoles = (...allowedRoles) => {
+  return (req, res, next) => {
+    if (!allowedRoles.includes(req.user.role)) {
+      return res
+        .status(403)
+        .json({ message: "Forbidden: You do not have permission" });
+    }
+    next();
+  };
+};
+
+module.exports = { authMiddleware, authorizeRoles };
